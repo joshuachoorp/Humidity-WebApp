bc8b7c2d16e8c8ab1bfd7173bb9182fcdaf5d0a2
Josh <joshuchoorp@gmail.com>
2023-10-05 16:18:26 +0800

    Commit

--- run.py
+++ run.py
@@ -10,27 +10,3 @@ from Requirements import checkReq
 
 
 
-if __name__ == '__main__':
-    while True:
-        # Checks for required packages and installs them if not found
-        try:
-            from app import app
-            app.run(debug=True)
-
-        # Checks for required packages and installs them if not found
-        # If module required not installed, will throw exception. 
-        # If thrown exception, will install modules required based on requirements.txt
-        except ModuleNotFoundError as e:
-            checkReq()    
-            print(e)     
-            continue
-
-        except Exception as e:
-            print(e)           
-            break
-        
-        break
-
-    
-
-        
