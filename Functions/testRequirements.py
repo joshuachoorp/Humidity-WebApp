1a1ada3f3af494795b0dbd20999a536dc1cd516d
zhiyi-laptop <>
2023-10-06 12:38:11 +0800

    checkReq update

---
+++ Functions/testRequirements.py
@@ -0,0 +1,25 @@
+import sys
+import subprocess
+import pkg_resources
+from pkg_resources import DistributionNotFound, VersionConflict
+
+
+
+# Check if all packages in requirements.txt is installed
+def checkReq():
+    try:
+        from Functions import dataGroup, linear_regression, correlation, overview_data, predictionHumidity
+        from distutils.command import upload
+        from flask import Flask, render_template, request, redirect, flash, send_file, send_from_directory, current_app, abort
+        import flask
+        import pandas as pd
+        import numpy as np
+
+
+    except Exception as e:
+        print(e)
+        requireFile = open('requirements.txt',mode='r')
+        requireList = requireFile.readlines()
+        requireFile.close()
+        for i in requireList:
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install', i])
