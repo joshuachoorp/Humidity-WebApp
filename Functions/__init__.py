3657bded746c9b83ede0072a6b8b9d7950adf6d1
zhiyi-school <144113528+zhiyi-school@users.noreply.github.com>
2023-10-12 23:27:39 +0800

    title fixes (#29)
    * 6-oct
    
    * checkReq update
    
    * fix merge conflict for requirements.txt
    
    * temp changes
    
    * changes to clean up some code
    
    * remove title from dataset
    
    * change small description to show overall data
    
    * new data implemented. display label for date needs adjusting
    
    * data is now dynamically retrieved by reading csv file and taking unique month and years.
    
    * new function to contain list of number of all graphs
    
    * dataCreateDiv can now take in region
    
    * commit
    
    * commit
    
    * able to view prediction and correlation graphs. unable to view overview graph
    
    * linear regression graph updated
    
    * new generation of region pages
    
    * prediction page testing
    
    * commit 9oct
    
    * prediction page now has all 4 graphs: prediction, correlation, overview and linear regression. will need to make nicer
    
    * prediction page now has all 4 graphs: prediction, correlation, overview and linear regression. will need to make nicer
    
    * edited calAvg in PlotWeather to convert all items in series into float before calculating mean. Cleaned up code as well
    
    * update
    
    * update
    
    * update a typo
    
    * weimings change
    
    * change dashboard.html name
    
    * change dashboard name to index
    
    * edit templates folder
    
    * template rename
    
    * static folder name change
    
    * change static folder name 2
    
    * update on downloads function
    
    * edit download to file path
    
    * changing of file names
    
    * changes to filenames
    
    * downloads folder
    
    * export and import files
    
    * debugging
    
    * debugging 2
    
    * export and table page file path chagnes
    
    * tables file path
    
    * final check
    
    * final check 0
    
    * east and west region typo
    
    * last changes
    
    * graph labels fixes
    
    * graph lebel fixes
    
    * title fixes
    
    * title fixes
    
    ---------
    
    Co-authored-by: zhiyi-laptop <>

--- Functions/__init__.py
+++ Functions/__init__.py
@@ -1,3 +1,3 @@
 from Functions.PlotWeather import dataPlot, dataGroup, canvasName, dataCreateDiv
-from Functions.predictionV4 import linear_regression, correlation, overview_data, predictionHumidity
+from Functions.prediction import linear_regression, correlation, overview_data, predictionHumidity
 from Functions.Filters import month_name_filter
